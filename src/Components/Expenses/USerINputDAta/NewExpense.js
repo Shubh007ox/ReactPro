0a6ba71bf6b157ff0adb14f641f376328888d552
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2022-10-06 04:45:50 -0700

    Add files via upload

---
+++ src/Components/Expenses/USerINputDAta/NewExpense.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm copy 2"
+import "./NewExpense.css";
+function NewExpense(){
+    return <div className="new-expense">
+        <ExpenseForm />
+    </div>
+
+
+     
+
+
+}
+export default NewExpense;

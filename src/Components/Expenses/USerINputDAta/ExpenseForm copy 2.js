23e4a57d5e47341050bcfccf3c28c7458746e78e
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2022-10-06 06:40:15 -0700

    Update ExpenseForm copy 2.js

--- src/Components/Expenses/USerINputDAta/ExpenseForm copy 2.js
+++ src/Components/Expenses/USerINputDAta/ExpenseForm copy 2.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseForm.css";
 function ExpenseForm() {
+  const [enterTitle,setUseState] = useState('')
+  const [enterAmount,setAmountstate] = useState('')
+  const [enterDate,setDatestate] = useState('')
+  
   function click4Handler(event) {
-    console.log(event.target.value);
+    setUseState(event.target.value);
+  }
+  function amountChangeHandler(event){
+    setAmountstate(event.target.value)
+  }
+  function dateChangeHandler(event){
+    setDatestate(event.target.value)
   }
   return (
     <div className="new-expense__controls">
@@ -13,11 +23,11 @@ function ExpenseForm() {
         </div>
         <div className="new-expense__controls">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"></input>
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}></input>
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31"></input>
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}></input>
         </div>
         <div className="new-expense__actions">
           <button type="submit" >Add Expenses</button>

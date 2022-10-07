9fc64c6851bf6ba471f31c4f9687c1a00174185c
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2022-10-07 01:54:34 -0700

    Update ExpenseForm copy 2.js

--- src/Components/Expenses/USerINputDAta/ExpenseForm copy 2.js
+++ src/Components/Expenses/USerINputDAta/ExpenseForm copy 2.js
@@ -1,22 +1,55 @@
 import React, {useState} from "react";
 import "./ExpenseForm.css";
 function ExpenseForm() {
+  // const [userInput,setUserInput] = useState({
+  //   enterTitle:'',
+  //   enterAmount:'',
+  //   enterDate:''
+  // })
   const [enterTitle,setUseState] = useState('')
   const [enterAmount,setAmountstate] = useState('')
   const [enterDate,setDatestate] = useState('')
   
   function click4Handler(event) {
-    setUseState(event.target.value);
+    setUseState(event.target.value)
+    // setUserInput({
+    //   ...userInput,
+    //   enterTitle:event.target.value
+    // })
+    // setUserInput((prevState) => {
+    //   return {...prevState, enterTitle:event.target.value}
+    // })
   }
   function amountChangeHandler(event){
     setAmountstate(event.target.value)
+    // console.log(enterAmount)
+    // setUserInput({
+    //   ...userInput,
+    //   enterAmount:event.target.value
+    // })
   }
   function dateChangeHandler(event){
     setDatestate(event.target.value)
+    // console.log(enterDate)
+    // setUserInput({
+    //   ...userInput,
+    //   enterDate:event.target.value
+    // })
+  }
+  function submitHandler(event){
+    event.preventDefault();
+    const expenseData = {
+    
+      title:enterTitle,
+      amount:enterAmount,
+      date: new Date(enterDate)
+    };
+    console.log(expenseData);
+
   }
   return (
     <div className="new-expense__controls">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <label>Title</label>
           <input type="text" onChange={click4Handler}></input>
